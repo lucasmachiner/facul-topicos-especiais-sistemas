4cca8525339ee6b65f726745e656edbbb9688199
Lucas Machiner <lucas.machiner@agileconnect.com.br>
2024-04-30 18:02:49 -0300

    avaliação 5, 6 e 9

---
+++ avaliacao/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Locadora locadora = new Locadora();
+Filme filme = new Filme();
+
+filme.Titulo = "Borat";
+filme.Genero = "Comedia";
+filme.Duracao = 1.60;
+filme.Disponivel = true;
+
+locadora.AdicionarFilme(filme);
+
+Filme filme1 = new Filme();
+
+filme1.Titulo = "Teste";
+filme1.Genero = "Ação";
+filme1.Duracao = 1.80;
+filme1.Disponivel = false;
+
+locadora.AdicionarFilme(filme1);
+
+Console.WriteLine("----------------- Locar filmes -----------------");
+locadora.LocarFilme("Borat");
+locadora.LocarFilme("Teste");
+
+
+Console.WriteLine("----------------- Devolver filmes -----------------");
+locadora.DevolverFilme("Borat");
